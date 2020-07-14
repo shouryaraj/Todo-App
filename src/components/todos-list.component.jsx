df9666dcda064411abab45f5b076c5d7f1fa9d6b
shourya <sraj0008@student.monash.edu>
2020-07-15 02:32:29 +1000

    Added delete functionality
     Still the data is rendering and fetching from the server, so need to delete the data from the server

--- src/components/todos-list.component.jsx
+++ src/components/todos-list.component.jsx
@@ -5,56 +5,62 @@ import Delete from './delete.jsx'
 
 
 
-const Todo = props => (
-    <tr>
-        <td className={props.todo.todo_completed ? 'completed': ''}>
-            <div class="card" ><div class="card-body">{props.todo.todo_description}
-            </div></div></td>
-        <td className={props.todo.todo_completed ? 'completed': ''}>
-                <div class="card"><div class="card-body">
-                {props.todo.todo_responsible}
-                </div></div></td>
-        <td className={props.todo.todo_completed ? 'completed': '' } >
-                <div class="card" id="priority"><div class="card-body">
-                {props.todo.todo_priority}
-                </div></div></td>
-        <td>
-
-            <Link to={"/edit/"+ props.todo._id}><div class="card"><div class="card-body">
-            <a class="icon 	fa fa-edit" id="uno" style={{}}></a>
-            
-                </div></div>
-                </Link>
-               
-    
-           
-        </td>
-        <td>
-{/* 
-        <div className="remove">
-            <a href="#" onClick={() => remove(id)}>X</a>
-        </div>
-                 */}
-
-
-        </td>
+const Todo = props => {
 
         
-        <td>
-                 <label style={{paddingLeft: "7px"}}>
-                <input type="checkbox" />
-                <span></span>
-                </label>
+     
+
+        return(
+            <tr>
+                <td className={props.todo.todo_completed ? 'completed': ''}>
+                    <div class="card" ><div class="card-body">{props.todo.todo_description}
+                    </div></div></td>
+                <td className={props.todo.todo_completed ? 'completed': ''}>
+                        <div class="card"><div class="card-body">
+                        {props.todo.todo_responsible}
+                        </div></div></td>
+                <td className={props.todo.todo_completed ? 'completed': '' } >
+                        <div class="card" id="priority"><div class="card-body">
+                        {props.todo.todo_priority}
+                        </div></div></td>
+                <td>
+
+                    <Link to={"/edit/"+ props.todo._id}><div class="card"><div class="card-body">
+                    <a class="icon 	fa fa-edit" id="uno" style={{}}></a>
+                    
+                        </div></div>
+                        </Link>
+                    
             
-            </td>
-    </tr>
-)
+                
+                </td>
+                <td>
+        
+                <div className="remove">
+                    <a href="#" onClick={() => props.remove(props.todo._id)}>X</a>
+                </div>
+                       
+
+
+                </td>
+
+                
+                <td>
+                        <label style={{paddingLeft: "7px"}}>
+                        <input type="checkbox" />
+                        <span></span>
+                        </label>
+                    
+                    </td>
+            </tr>
+        )}
 
 
 class TodoList extends Component {
     constructor(props){
         super(props);
         this.state ={todos: []};
+        this.removeRow = this.removeRow.bind(this)
     }
 
     componentDidMount(){
@@ -87,13 +93,26 @@ class TodoList extends Component {
         });
     }
     
+    removeRow = (rowId) => {
+        // Array.prototype.filter returns new array
+        // so we aren't mutating state here
+        console.log(this.state.todos)
+        console.log(rowId)
+        const arrayCopy = this.state.todos.filter((row) => row._id !== rowId);
+        this.setState({todos: arrayCopy});
 
+
+
+        
+        console.log(this.state.todos)
+      };
     todoList() {
-        return this.state.todos.map(function(currentTodo, i){
-            
-            return <Todo todo = {currentTodo} key={i}/>    
+        return this.state.todos.map((currentTodo, i)=>{
+          
+            return <Todo todo={currentTodo} remove={this.removeRow} key={i} />    
         });
     }
+
 
     render (){
         return(
